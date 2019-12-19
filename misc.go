8aca2e730afc210ab5c7a1ba4d2ff7ba6d9dacea
shaoying <shylinux@icloud.com>
2019-12-19 09:58:23 +0800

    add Time

--- misc.go
+++ misc.go
@@ -3,6 +3,9 @@ package kit
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"path"
@@ -70,6 +73,31 @@ func Create(p string) (*os.File, string, error) {
 	f, e := os.Create(p)
 	return f, p, e
 }
+func Time(arg ...string) int {
+	if len(arg) == 0 {
+		return Int(time.Now())
+	}
+
+	if len(arg) > 1 {
+		if t, e := time.ParseInLocation(arg[1], arg[0], time.Local); e == nil {
+			return Int(t)
+		}
+	}
+
+	for _, v := range []string{
+		"2006-01-02 15:04:05",
+		"2006-01-02 15:04",
+		"01-02 15:04",
+		"2006-01-02",
+		"2006/01/02",
+		"15:04:05",
+	} {
+		if t, e := time.ParseInLocation(v, arg[0], time.Local); e == nil {
+			return Int(t)
+		}
+	}
+	return 0
+}
 func Duration(str interface{}) time.Duration {
 	switch str := str.(type) {
 	case string:
@@ -80,4 +108,46 @@ func Duration(str interface{}) time.Duration {
 }
 func Width(str string, mul int) int {
 	return len([]rune(str)) + (len(str)-len([]rune(str)))/2/mul
+}
+func FmtSize(size int64) string {
+	if size > 1<<30 {
+		return fmt.Sprintf("%d.%dG", size>>30, (size>>20)%1024*100>>10)
+	}
+
+	if size > 1<<20 {
+		return fmt.Sprintf("%d.%dM", size>>20, (size>>10)%1024*100>>10)
+	}
+
+	if size > 1<<10 {
+		return fmt.Sprintf("%d.%dK", size>>10, size%1024*100>>10)
+	}
+
+	return fmt.Sprintf("%dB", size)
+}
+func FmtTime(t int64) string {
+	sign, time := "", t
+	if time < 0 {
+		sign, time = "-", -t
+	}
+	if time > 1000000000 {
+		return fmt.Sprintf("%s%d.%ds", sign, time/1000000000, (time/1000000)%1000*100/1000)
+	}
+	if time > 1000000 {
+		return fmt.Sprintf("%s%d.%dms", sign, time/1000000, (time/1000)%1000*100/1000)
+	}
+	if time > 1000 {
+		return fmt.Sprintf("%s%d.%dus", sign, time/1000, (time/1000)%1000*100/1000)
+	}
+	return fmt.Sprintf("%s%dns", sign, time)
+}
+func UnMarshal(data string) interface{} {
+	var res interface{}
+	if strings.HasSuffix(data, ".json") {
+		if b, e := ioutil.ReadFile(data); e == nil {
+			json.Unmarshal(b, &res)
+		}
+	} else {
+		json.Unmarshal([]byte(data), &res)
+	}
+	return res
 }
