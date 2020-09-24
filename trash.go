8fa650a037801dc1f94ec90df1cf48ece03e9ef7
shaoying <shylinux@icloud.com>
2020-09-24 23:49:01 +0800

    opt some

---
+++ trash.go
@@ -0,0 +1,13 @@
+package kit
+
+const (
+	// MDB_LIST = "list"
+	MDB_SHOW  = "show"
+	MDB_SYNC  = "sync"
+	MDB_PULL  = "pull"
+	MDB_PUSH  = "push"
+	MDB_PROXY = "proxy"
+	MDB_SHARE = "share"
+
+	MDB_INPUT = "_input"
+)
