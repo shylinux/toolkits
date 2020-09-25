621190494d84af4895a3bfcc180339de2cdf9b90
shaoying <shaoying@zuoyebang.com>
2020-09-25 19:49:17 +0800

    opt some

---
+++ exec.go
@@ -0,0 +1,15 @@
+package kit
+
+const (
+	GDB_OPEN  = "open"
+	GDB_CLOSE = "close"
+	GDB_START = "start"
+	GDB_STOP  = "stop"
+)
+
+const (
+	GDB_POD = "pod"
+	GDB_CTX = "ctx"
+	GDB_CMD = "cmd"
+	GDB_ARG = "arg"
+)
