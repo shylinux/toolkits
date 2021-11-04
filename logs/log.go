4fac872f95672365ec6df3361461f95c975bab85
shylinux <shylinux@163.com>
2021-11-04 15:08:54 +0800

    add m1

--- logs/log.go
+++ logs/log.go
@@ -83,7 +83,7 @@ var trans = map[string]string{
 	"yellow": "\033[33m",
 }
 
-var LogDisable = false
+var LogDisable = true
 
 func (log *Log) output(level string, arg ...interface{}) bool {
 	if LogDisable {
