d84f47ac3fc43909a4140c9c3a2e3f1df1c5aa7d
harveyshao <harveyshao@tencent.com>
2021-12-09 00:46:36 +0800

    opt chart

--- core.go
+++ core.go
@@ -14,60 +14,55 @@ import (
 )
 
 func _list(str string) map[rune]bool {
-	sep := map[rune]bool{}
+	space := map[rune]bool{}
 	for _, c := range str {
-		sep[c] = true
+		space[c] = true
 	}
-	return sep
+	return space
 }
 func Split(str string, arg ...string) (res []string) {
-	// 空白符
-	sep := _list(Select("\t ,\n", arg, 0))
-	// 分隔符
-	sup := _list(Select("{[()]}", arg, 1))
-	// 引用符
-	sub := _list(Select("\"'`", arg, 2))
+	space := _list(Select("\t ,\n", arg, 0)) // 空白符
+	block := _list(Select("{[()]}", arg, 1)) // 分隔符
+	quote := _list(Select("\"'`", arg, 2))   // 引用符
 
-	// 开始分词
 	list := []rune(str)
-	left, space, begin := '\000', true, 0
+	left, void, begin := '\000', true, 0
 	for i := 0; i < len(list); i++ {
 		switch {
-		case sep[list[i]]: // 空白符
+		case space[list[i]]: // 空白符
 			if left == '\000' {
-				if !space {
+				if !void {
 					res = append(res, string(list[begin:i]))
 				}
-				space, begin = true, i+1
+				void, begin = true, i+1
 			}
-		case sub[list[i]]: // 引用符
+		case quote[list[i]]: // 引用符
 			if left == '\000' {
-				left, space, begin = list[i], false, i+1
+				left, void, begin = list[i], false, i+1
 			} else if left == list[i] {
 				res = append(res, string(list[begin:i]))
-				left, space, begin = '\000', true, i+1
+				left, void, begin = '\000', true, i+1
 			}
-		case sup[list[i]]: // 分隔符
+		case block[list[i]]: // 分隔符
 			if left == '\000' {
-				if !space {
+				if !void {
 					res = append(res, string(list[begin:i]))
 				}
 				res = append(res, string(list[i:i+1]))
-				space, begin = true, i+1
+				void, begin = true, i+1
 			}
 		case list[i] == '\\': // 转义符
 			for i := i; i < len(list)-1; i++ {
 				list[i] = list[i+1]
 			}
 			list = list[:len(list)-1]
-			space = false
+			void = false
 		default:
-			space = false
+			void = false
 		}
 	}
 
-	// 末尾字符
-	if begin < len(list) {
+	if begin < len(list) { // 末尾字符
 		res = append(res, string(list[begin:]))
 	}
 	return res
